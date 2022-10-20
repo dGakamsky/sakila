54d34284059e745478ef411bff8fd5da25e0a025
dGakamsky <46941271+dGakamsky@users.noreply.github.com>
2022-10-20 15:49:14 +0100

    page works

--- src/main/java/sakila_api/sakila/SakilaApplication.java
+++ src/main/java/sakila_api/sakila/SakilaApplication.java
@@ -1,13 +1,38 @@
 package sakila_api.sakila;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@RestController
+@CrossOrigin
+@RequestMapping("/home")
+
 public class SakilaApplication {
+
+	@Autowired
+
+	private ActorRepository actorRepo;
+
+	public SakilaApplication(ActorRepository exActorRepo){
+		this.actorRepo = exActorRepo;
+	}
+
+
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SakilaApplication.class, args);
 	}
+
+	@GetMapping("/allActors")
+	public @ResponseBody
+	Iterable<Actor> getAllActors(){
+		return actorRepo.findAll();
+	}
+
 
 }
