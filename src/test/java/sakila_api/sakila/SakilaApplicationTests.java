f22687c28b9e27a46475f2d6d04d0505fb22977b
dGakamsky <46941271+dGakamsky@users.noreply.github.com>
2022-10-20 12:07:37 +0100

    sakila

---
+++ src/test/java/sakila_api/sakila/SakilaApplicationTests.java
@@ -0,0 +1,13 @@
+package sakila_api.sakila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SakilaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
