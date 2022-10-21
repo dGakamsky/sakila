0ba03bf7673ab8f497af1b222fb8d4c117e8b5d5
dGakamsky <46941271+dGakamsky@users.noreply.github.com>
2022-10-21 09:37:46 +0100

    added Film

--- src/main/java/sakila_api/sakila/MovieRepository.java
+++
@@ -1,7 +0,0 @@
-package sakila_api.sakila;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieRepository extends JpaRepository<Movie, Integer> {
-}
